d1acba60160910eda2084584bbb54e46ba702c8b
miguelnavarro06 <miguela.navarror@gmail.com>
2025-10-24 00:12:40 -0600

    Correccion del diagrama de dispersion

--- aplicacion_web.py
+++ aplicacion_web.py
@@ -7,14 +7,14 @@ st.header('Aplicacion basada en Streamlit')
 
 hist_button = st.button('Construir histograma')
 if hist_button:
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un histograma de la distribucion del odometro')
     fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
     fig.update_layout(title_text='Distribución del Odómetro')
     st.plotly_chart(fig, use_container_width=True)
 
 scattering_button = st.button('Construir diagrama de dispersion')
 if scattering_button:
-    st.write('Creación de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
-    fig = go.Figure(data=[go.Scatter(x=car_data['odometer'])])
-    fig.update_layout(title_text='Distribución del Odómetro')
+    st.write('Creación de un diagrama de dispersion que muestra la relacion entre el odometro y el precio')
+    fig = go.Figure(data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+    fig.update_layout(title_text='Relacion odometro vs precio')
     st.plotly_chart(fig, use_container_width=True)
