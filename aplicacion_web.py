f0f51abb05432c2c640ffb16a39f3c9d04bfd5c2
miguelnavarro06 <miguela.navarror@gmail.com>
2025-10-23 21:25:18 -0600

    carga de archivos

---
+++ aplicacion_web.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import streamlit as st
+import plotly.graph_objects as go
+car_data = pd.read_csv('C:/Users/angee/OneDrive/Documentos/proyecto sprint 7/proyecto_sprint_7/vehicles_us.csv')
+st.header('Aplicacion basada en Streamlit')
+
+
+hist_button = st.button('Construir histograma')
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+    fig.update_layout(title_text='Distribución del Odómetro')
+    st.plotly_chart(fig, use_container_width=True)
+
+scattering_button = st.button('Construir diagrama de dispersion')
+if scattering_button:
+    st.write('Creación de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches')
+    fig = go.Figure(data=[go.Scatter(x=car_data['odometer'])])
+    fig.update_layout(title_text='Distribución del Odómetro')
+    st.plotly_chart(fig, use_container_width=True)
